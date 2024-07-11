49425d612a055d20a2829b442206d53d81999e70
Mihail <welcomepictogram@gmail.com>
2024-07-12 00:24:49 +0300

    Добавлен обработчик для корневого маршрута

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ app.use(bodyParser.json());
 const TELEGRAM_API_TOKEN = '7301096593:AAH9Jcvg6ucTK8txyMB1xiNlhuPa6SRw0GA';
 const TELEGRAM_API_URL = `https://api.telegram.org/bot${TELEGRAM_API_TOKEN}`;
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
 app.post('/webhook', (req, res) => {
   const message = req.body.message;
 
